88d113ee7974e8d62e064e0972b514b8cb35d3aa
Robert Anderson <robert@robertanderson.dev>
2024-06-26 12:50:55 -0600

    Added tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,63 @@
+import os
 import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+import pandas as pd
+from pathlib import Path
+from ml.model import train_model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+@pytest.fixture
+def census_file() -> Path:
+    return Path(__file__).parent.resolve() / 'data' / 'census.csv'
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+# First test
+def test_train_test_split_size(census_file):
     """
-    # add description for the third test
+    Assert that the train_test_split function slices the census data into a suitable size.
     """
-    # Your code here
-    pass
+    data = pd.read_csv(str(census_file))
+    train, test = train_test_split(data, test_size = 0.2)
+    assert len(test) >= 2000
+
+
+# Second test
+def test_census_column_names(census_file):
+    """
+    Assert that all expected columns are present in the census file.
+    """
+    data = pd.read_csv(census_file)
+
+    features = {
+        'age',
+        'workclass',
+        'fnlgt',
+        'education',
+        'education-num',
+        'marital-status',
+        'occupation',
+        'relationship',
+        'race',
+        'sex',
+        'capital-gain',
+        'capital-loss',
+        'hours-per-week',
+        'native-country',
+        'salary'
+    }
+
+    assert set(data.columns) == features
+
+
+# Third test
+def test_train_model_return_type():
+    """
+    # Check that the model is the RandomForestClassifier.
+    """
+    sample_x = [[0, 1, 2], [3, 4, 5]]
+    sample_y = ['col1', 'col2']
+
+    model = train_model(sample_x, sample_y)
+
+    assert isinstance(model, RandomForestClassifier)
